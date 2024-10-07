c52e340cb7e875f9a8f22b623ef8a05520b27432
Steffew <xgenyofficial@gmail.com>
2024-10-07 21:43:39 +0200

    chore(upload-page): reduce upload box div padding

--- src/app/routes/app/upload.tsx
+++ src/app/routes/app/upload.tsx
@@ -1,7 +1,7 @@
 export const UploadRoute = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-blue-900 py-12 sm:px-6 lg:px-8">
-      <div className="w-full max-w-md rounded-lg bg-gray-100 p-8 shadow-lg">
+      <div className="w-full max-w-md rounded-lg bg-gray-100 p-5 shadow-lg">
         <div className="cursor-pointer border-2 border-dashed border-gray-400 p-8 text-center">
           <span className="material-symbols-outlined text-6xl text-gray-500">
             upload
