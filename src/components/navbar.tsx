693bed281d1971cc9f118dcbe1833ac65dab08c9
Flavio Murata <flaviomurata02@gmail.com>
2024-10-10 17:23:57 -0300

    feat(auth): implement authentication logic

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,23 +2,23 @@ import { Link } from 'react-router-dom';
 
 export const NavBar = () => {
   return (
-    <nav className="fixed top-4 left-4 bg-gray-200 text-white rounded-full p-2 shadow-lg z-50">
+    <nav className="fixed left-4 top-4 z-50 rounded-full bg-gray-200 p-2 text-white shadow-lg">
       <div className="flex space-x-4 px-2">
         <Link
           to="/"
-          className="transition-colors duration-300 text-gray-800 hover:text-black"
+          className="text-gray-800 transition-colors duration-300 hover:text-black"
         >
           Home
         </Link>
         <Link
           to="/login"
-          className="transition-colors duration-300 text-gray-800 hover:text-black"
+          className="text-gray-800 transition-colors duration-300 hover:text-black"
         >
           Login
         </Link>
         <Link
           to="/app"
-          className="transition-colors duration-300 text-gray-800 hover:text-black"
+          className="text-gray-800 transition-colors duration-300 hover:text-black"
         >
           App
         </Link>
