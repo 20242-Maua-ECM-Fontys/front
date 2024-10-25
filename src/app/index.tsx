e64f5b7eec52daa82c0a14d6ea56f6c51607be38
Pedro Matumoto <pedromatumoto@gmail.com>
2024-10-25 17:28:32 -0300

    feat(entra-id): add instance on provider

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,22 @@
 import { AppProvider } from './provider';
 import { AppRouter } from './router';
 
+import { PublicClientApplication, EventType } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+import { msalConfig } from 'src/components/layouts/auth-config';
+
+
+const msalInstance = new PublicClientApplication(msalConfig);
+msalInstance.initialize();
+
+
+
 export const App = () => {
   return (
-    <AppProvider>
-      <AppRouter />
-    </AppProvider>
+    <MsalProvider instance={msalInstance}>
+      <AppProvider>
+        <AppRouter />
+      </AppProvider>
+    </MsalProvider>
   );
 };
