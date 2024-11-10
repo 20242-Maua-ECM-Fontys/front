0215fd4bbfacf9f4fc10d88c2c19ea481b2ebf36
Pedro Matumoto <pedromatumoto@gmail.com>
2024-11-10 11:39:38 -0300

    feat(suit-avail-teachers): integration with backend API

--- src/app/routes/app/teacher-suitability-availability.tsx
+++ src/app/routes/app/teacher-suitability-availability.tsx
@@ -1,253 +1,229 @@
-  import { ContentLayout } from '@/components/layouts';
-  import { ScrollArea } from '@/components/ui/scroll-area';
-  import WeekAvailability from '@/components/ui/week-availability';
-  import { useMsal} from '@azure/msal-react';
-  import { useEffect, useState } from 'react';
-  import {
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+import {
   Command,
-  CommandDialog,
   CommandEmpty,
   CommandGroup,
   CommandInput,
   CommandItem,
   CommandList,
-  CommandSeparator,
-  CommandShortcut,
-} from "@/components/ui/command"
+} from '@/components/ui/command';
+import WeekAvailability from '@/components/ui/week-availability-update';
+import { toast } from '@/hooks/use-toast';
+import { useUser } from '@/hooks/use-user';
 
+export const TeacherSuitabilityAndAvailabilityRoute = () => {
+  interface Availability {
+    startTime: number; // em minutos
+    endTime: number; // em minutos
+    weekDay: string;
+  }
+  const [weekKey] = useState(0);
+  const [selectedSubjects, setSelectedSubjects] = useState<string[]>([]);
+  const [subjects, setSubjects] = useState<Subject[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [availability, setAvailability] = useState<Availability[]>([]);
+  const { userId } = useUser();
 
-  export const TeacherSuitabilityAndAvailabilityRoute = () => {
-    const {instance, accounts} = useMsal();
-    const [weekKey, setWeekKey] = useState(0);
-    const [selectedSubjects, setSelectedSubjects] = useState<string[]>([]);
+  interface Subject {
+    codeSubject: string;
+    subjectName: string;
+    period: string;
+  }
 
-    const handleSubjectAdd = (subject: string) => {
-      if (selectedSubjects.includes(subject)) {
-        setSelectedSubjects(selectedSubjects.filter((s) => s !== subject));
-        return;
-      }
-      setSelectedSubjects([...selectedSubjects, subject]);
+  const handlegetSubjects = async () => {
+    try {
+      const response = await axios.get(
+        import.meta.env.VITE_APP_API_URL + 'get_all_subjects',
+      );
+      setSubjects(response.data.subjects);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const subjects: { [key: string]: string[] }[] = [
-      {
-        'Computer Engineering': [
-          'Computer Architecture',
-          'Computer Networks',
-          'Computer Programming',
-          'Computer Security',
-          'Data Structures',
-          'Databases',
-          'Digital Logic',
-          'Discrete Mathematics',
-          'Operating Systems',
-          'Software Engineering',
-        ]
-      },
-      {
-        'Electrical Engineering': [
-          'Analog Electronics',
-          'Circuit Analysis',
-          'Control Systems',
-          'Digital Electronics',
-          'Digital Signal Processing',
-          'Electromagnetics',
-          'Electronics',
-          'Linear Systems',
-          'Power Systems',
-          'Signals and Systems',
-        ]
-      },
-      {
-        'Mechanical Engineering': [
-          'Dynamics',
-          'Engineering Mechanics',
-          'Fluid Mechanics',
-          'Heat Transfer',
-          'Machine Design',
-          'Materials Science',
-          'Mechanics of Materials',
-          'Thermodynamics',
-          'Vibrations',
-        ]
-      },
-      {
-      'Civil Engineering': [
-        'Concrete Design',
-        'Construction Management',
-        'Environmental Engineering',
-        'Geotechnical Engineering',
-        'Hydraulics',
-        'Materials Testing',
-        'Reinforced Concrete',
-        'Steel Design',
-        'Structural Analysis',
-        'Surveying',
-        ]
-      },
-      {
-        'Chemical Engineering': [
-          'Chemical Kinetics',
-          'Chemical Process Control',
-          'Chemical Reaction Engineering',
-          'Chemical Thermodynamics',
-          'Fluid Mechanics',
-          'Heat Transfer',
-          'Mass Transfer',
-          'Materials Science',
-          'Process Design',
-          'Transport Phenomena',
-        ]
-      },
-      {
-        'Biomedical Engineering': [
-          'Biomechanics',
-          'Biomedical Instrumentation',
-          'Biomedical Signal Processing',
-          'Biomaterials',
-          'Biomechanics',
-          'Biomedical Instrumentation',
-          'Biomedical Signal Processing',
-          'Biomaterials',
-          'Biomedical Imaging',
-          'Physiology',
-        ]
-      },
-      {
-        'Aerospace Engineering': [
-          'Aerodynamics',
-          'Aerospace Materials',
-          'Aircraft Design',
-          'Aircraft Structures',
-          'Astrodynamics',
-          'Flight Dynamics',
-          'Propulsion',
-          'Spacecraft Design',
-          'Spacecraft Structures',
-          'Thermodynamics',
-        ]
-      },
-      {
-        'Industrial Engineering': [
-          'Engineering Management',
-          'Engineering Statistics',
-          'Facilities Planning',
-          'Human Factors Engineering',
-          'Industrial Automation',
-          'Industrial Engineering',
-          'Manufacturing Processes',
-          'Operations Research',
-          'Quality Control',
-          'Supply Chain Management',
-        ]
-      },
-      {
-        'Software Engineering': [
-          'Agile Software Development',
-          'Computer Programming',
-          'Data Structures',
-          'Databases',
-          'Operating Systems',
-          'Software Architecture',
-          'Software Design',
-          'Software Engineering',
-          'Software Testing',
-          'Web Development',
-        ]
-      },
-    ]
+  const handleAvailabilityChange = (newAvailability: Availability[]) => {
+    setAvailability(newAvailability);
+  };
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
+  const handleSubjectAdd = (subject: string) => {
+    if (selectedSubjects.includes(subject)) {
+      setSelectedSubjects(selectedSubjects.filter((s) => s !== subject));
+      return;
+    }
+    setSelectedSubjects([...selectedSubjects, subject]);
+    console.log(selectedSubjects);
+  };
 
-    return (
-      <div>
-        <div className="flex h-screen items-center">
-          <div className="mx-auto max-w-7xl px-4 py-12 text-center sm:px-6 lg:px-8 lg:py-16">
-            <h2 className="p-4 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-              <span className="block">Availability and Suitability</span>
-            </h2>
-            <div className="flex gap-4 justify-center items-center">
-              <button className="mt-4 rounded bg-gray-200 px-4 py-2  transition duration-300 hover:bg-blue-400 hover:text-white"
-                          onClick={() => document
-                        .getElementById('subject-possibilities')
-                        ?.scrollIntoView({ behavior: 'smooth' })}>Fill suitabilities</button>
-              <button className="mt-4 rounded bg-gray-200 px-4 py-2  transition duration-300 hover:bg-blue-400 hover:text-white"
-                          onClick={() => document
-                        .getElementById('table-possibilities')
-                        ?.scrollIntoView({ behavior: 'smooth' })}>Fill availabilities</button>
-            </div>
+  const handleUpdateSubjects = async (selectedSubjects: string[]) => {
+    try {
+      const response = await axios.post(
+        import.meta.env.VITE_APP_API_URL + 'update_suitabilities',
+        {
+          userId: userId,
+          suitabilities: selectedSubjects,
+        },
+      );
+      toast({
+        title: 'Success',
+        description: 'Subjects updated successfully',
+      });
+      console.log(response);
+    } catch (error) {
+      toast({
+        variant: 'destructive',
+        title: 'Error',
+        description: 'Error updating subjects',
+      });
+      console.log(error);
+    }
+  };
+
+  const handleUpdateAvailability = async (availability: Availability[]) => {
+    try {
+      const response = await axios.post(
+        import.meta.env.VITE_APP_API_URL + 'update_availabilities',
+        {
+          userId: userId,
+          availabilities: availability,
+        },
+      );
+      toast({
+        title: 'Success',
+        description: 'Availability updated successfully',
+      });
+      console.log(response);
+    } catch (error) {
+      toast({
+        variant: 'destructive',
+        title: 'Error',
+        description: 'Error updating availability',
+      });
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    handlegetSubjects();
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div>
+      <div className="flex h-screen items-center">
+        <div className="mx-auto max-w-7xl px-4 py-12 text-center sm:px-6 lg:px-8 lg:py-16">
+          <h2 className="p-4 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            <span className="block">Availability and Suitability</span>
+          </h2>
+          <div className="flex items-center justify-center gap-4">
+            <button
+              className="mt-4 rounded bg-gray-200 px-4 py-2 transition duration-300 hover:bg-blue-400 hover:text-white"
+              onClick={() =>
+                document
+                  .getElementById('subject-possibilities')
+                  ?.scrollIntoView({ behavior: 'smooth' })
+              }
+            >
+              Fill suitabilities
+            </button>
+            <button
+              className="mt-4 rounded bg-gray-200 px-4 py-2 transition duration-300 hover:bg-blue-400 hover:text-white"
+              onClick={() =>
+                document
+                  .getElementById('table-possibilities')
+                  ?.scrollIntoView({ behavior: 'smooth' })
+              }
+            >
+              Fill availabilities
+            </button>
           </div>
         </div>
-        <div id="subject-possibilities" className='min-h-screen'>
-          <div className="mx-auto max-w-7xl px-4 py-12 text-center sm:px-6 lg:px-8 lg:py-16">
-              <h2 className="text-2xl font-semibold p-4">Selected Subjects:</h2>
-              <div className="flex flex-wrap gap-2 justify-center text-center">
-                {selectedSubjects.length === 0 ? (
-                  <span className="rounded bg-gray-200 px-3 py-1 text-gray-800">
-                    No subjects selected
-                  </span>
-                ) : (
-                  selectedSubjects.map((subject) => (
-                    <span
-                      key={subject}
-                      className="rounded bg-green-400 px-3 py-1 flex items-center gap-2"
-                    >
-                      {subject}
-                    </span>
-                  ))
-                )}
-              </div>
-            </div>
-
-          <Command className='min-h-[49vh]'> 
-              <CommandInput placeholder="Type a subject name..." />
-              <CommandList>
-                <CommandEmpty>No results found.</CommandEmpty>
-                <CommandGroup heading="Subjects">
-                  {
-                    // fazer dois for, um para os cursos e outro para as disciplinas
-                    subjects.map((subject) => (
-                      Object.keys(subject).map((course) => (
-                        subject[course].map((subjectName) => (
-                          <CommandItem key={subjectName}
-                            onSelect={() => {handleSubjectAdd(subjectName);}}
-                            className={`p-2 transition duration-300 flex items-center justify-center cursor-pointer ${
-                              selectedSubjects.includes(subjectName)
-                                ? 'bg-blue-400 text-white'
-                                : 'hover:bg-blue-400 hover:text-white'
-                            }`}
-                            >
-                            {subjectName}
-                          </CommandItem>
-                        ))
-                      ))
-                    ))
-                  }
-                  
-                </CommandGroup>
-              </CommandList>
-            </Command>
-          
-
-        </div>
-        
-        <div
-          className="mx-auto flex h-screen max-w-7xl flex-col items-center justify-center p-4 text-center sm:px-6 lg:px-8 lg:py-10 mt-10 md:mt-0"
-          id="table-possibilities"
-        >
-          <h3 className="mb-4 text-center text-xl font-bold">
-            Set Your Availability
-          </h3>
-          <WeekAvailability
-            startHour={'07:40'}
-            endHour={'22:20'}
-            key={weekKey}
-          />
+      </div>
+      <div id="subject-possibilities" className="min-h-screen">
+        <div className="mx-auto max-w-7xl px-4 py-12 text-center sm:px-6 lg:px-8 lg:py-16">
+          <h2 className="p-4 text-2xl font-semibold">Selected Subjects:</h2>
+          <div className="flex flex-wrap justify-center gap-2 text-center">
+            {selectedSubjects.length === 0 ? (
+              <span className="rounded bg-gray-200 px-3 py-1 text-gray-800">
+                No subjects selected
+              </span>
+            ) : (
+              selectedSubjects.map((subject) => (
+                <span
+                  key={subject}
+                  className="flex items-center gap-2 rounded bg-green-400 px-3 py-1"
+                >
+                  {subject}
+                </span>
+              ))
+            )}
+          </div>
         </div>
 
+        {loading ? (
+          <div className="flex h-[49vh] items-center justify-center">
+            <div className="size-32 animate-spin rounded-full border-y-2 border-gray-900"></div>
+          </div>
+        ) : (
+          <Command className="min-h-[49vh]">
+            <CommandInput placeholder="Type a subject name..." />
+            <CommandList>
+              <CommandEmpty>No results found.</CommandEmpty>
+              <CommandGroup heading="Subjects">
+                {subjects.map((subject: Subject) => (
+                  <CommandItem
+                    key={subject.codeSubject}
+                    onSelect={() => handleSubjectAdd(subject.codeSubject)}
+                  >
+                    {subject.subjectName}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        )}
+        <div className="flex justify-center">
+          <button
+            className="mt-4 rounded bg-gray-200 px-4 py-2 transition duration-300 hover:bg-blue-400 hover:text-white"
+            onClick={() => {
+              document
+                .getElementById('table-possibilities')
+                ?.scrollIntoView({ behavior: 'smooth' });
+              handleUpdateSubjects(selectedSubjects);
+            }}
+          >
+            Save
+          </button>
+        </div>
       </div>
 
-    );
-  };
+      <div
+        className="mx-auto mt-10 flex h-screen max-w-7xl flex-col items-center justify-center p-4 text-center sm:px-6 md:mt-0 lg:px-8 lg:py-10"
+        id="table-possibilities"
+      >
+        <h3 className="mb-4 text-center text-xl font-bold">
+          Set Your Availability
+        </h3>
+        <WeekAvailability
+          startHour={'07:40'}
+          endHour={'22:20'}
+          key={weekKey}
+          initialAvailability={availability}
+          onAvailabilityChange={handleAvailabilityChange}
+        />
+        <div className="flex justify-center">
+          <button
+            className="mt-4 rounded bg-gray-200 px-4 py-2 transition duration-300 hover:bg-blue-400 hover:text-white"
+            onClick={() => {
+              handleUpdateAvailability(availability);
+            }}
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
