38d95672d1b8911f916fa6e9f728732b94c8364a
Flavio Murata <flaviomurata02@gmail.com>
2024-11-12 01:00:06 -0300

    refactor(auth): move msal to app provider

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,22 +1,10 @@
 import { AppProvider } from './provider';
 import { AppRouter } from './router';
 
-import { PublicClientApplication, EventType } from '@azure/msal-browser';
-import { MsalProvider } from '@azure/msal-react';
-import { msalConfig } from 'src/components/layouts/auth-config';
-
-
-const msalInstance = new PublicClientApplication(msalConfig);
-msalInstance.initialize();
-
-
-
 export const App = () => {
   return (
-    <MsalProvider instance={msalInstance}>
-      <AppProvider>
-        <AppRouter />
-      </AppProvider>
-    </MsalProvider>
+    <AppProvider>
+      <AppRouter />
+    </AppProvider>
   );
 };
