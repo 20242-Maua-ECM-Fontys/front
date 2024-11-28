037eb903bc9f8a08090f00f7a0004185ee446c74
MichaX5 <micha@vanzutphen.info>
2024-11-28 14:12:51 +0100

    chore(login) Changed login colors to fit with the style of the other pages

--- src/components/layouts/auth-layout.tsx
+++ src/components/layouts/auth-layout.tsx
@@ -2,6 +2,7 @@ import { useMsal } from '@azure/msal-react';
 import * as React from 'react';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Circles from '@/assets/images/Circles.svg';
 
 import logo from '@/assets/images/MauaGrid-logo.svg';
 import { Head } from '@/components/seo';
@@ -25,21 +26,30 @@ export const AuthLayout = ({ children, title }: LayoutProps) => {
   return (
     <>
       <Head title={title} />
-      <div className="flex min-h-screen flex-col justify-center bg-blue-800 sm:px-6 lg:px-8">
-        <div className="flex flex-col items-center sm:mx-auto sm:w-full sm:max-w-md">
-          <Link className="flex items-center text-white" to="/">
-            <img className="h-24 w-auto" src={logo} alt="Workflow" />
-          </Link>
-          <h2 className="w-80 text-center text-xl font-extrabold leading-relaxed text-white sm:w-[50rem] sm:leading-normal">
-            Building your Schedules in a straightforward manner
-          </h2>
-        </div>
-        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-          <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
-            {children}
+      <div className="flex min-h-screen flex-col items-center justify-center sm:px-6 lg:px-8">
+        <div className=" w-[40rem] h-[25rem] rounded-2xl drop-shadow-[0px_0px_10px_rgba(0,0,0,0.25)] flex-col flex items-center justify-center">
+          <div className="flex flex-col items-center sm:mx-auto sm:w-full sm:max-w-md">
+            <Link className="flex items-center" to="/">
+              <img className="h-24 w-auto" src={logo} alt="Workflow" />
+            </Link>
+            <h2
+              className="w-80 text-center text-xl font-extrabold leading-relaxed text-gray-500 sm:w-[50rem] sm:leading-normal">
+              Building your Schedules in a straightforward manner
+            </h2>
+          </div>
+          <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+            <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+              {children}
+            </div>
           </div>
         </div>
       </div>
+      {/* Decorative circles positioned under the login form */}
+      <img
+        src={Circles}
+        className="absolute bottom-0 left-5 w-96 rotate-180"
+        alt="Decorative circles"
+      />
     </>
   );
 };
--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -1,4 +1,3 @@
-import Circles from '@/assets/images/Circles.svg';
 import { Button } from '@/components/ui/button';
 
 type LoginFormProps = {
@@ -8,12 +7,6 @@ type LoginFormProps = {
 export const LoginForm = ({ onSuccess }: LoginFormProps) => {
   return (
     <div className="flex flex-col items-center">
-      {/* Decorative circles positioned under the login form */}
-      <img
-        src={Circles}
-        className="absolute bottom-0 left-5 w-96 rotate-180"
-        alt="Decorative circles"
-      />
       <div className="z-10 w-full max-w-sm">
         <Button onClick={onSuccess} className="w-full">
           Log in with your Microsoft Account
@@ -21,7 +14,7 @@ export const LoginForm = ({ onSuccess }: LoginFormProps) => {
       </div>
       <div className="z-10 mt-2 flex items-center justify-end">
         <div className="text-sm">
-          <span className="font-medium text-blue-600">
+          <span className="font-medium text-gray-400">
             Need an account? Please contact a staff member.
           </span>
         </div>
