f4d6c4e4d65932a7fe0c49e39ee7551b8aa2eef6
MichaX5 <micha@vanzutphen.info>
2024-11-28 13:38:04 +0100

    fix(login&fonts) Remove font's & handlelogout function

--- src/app/routes/auth/login.tsx
+++ src/app/routes/auth/login.tsx
@@ -13,10 +13,6 @@ export const LoginRoute = () => {
     instance.loginPopup().catch((e) => console.error(e));
   };
 
-  const handleLogout = () => {
-    instance.logoutPopup().catch((e) => console.error(e));
-  };
-
   useEffect(() => {
     if (accounts.length > 0) {
       navigate('/app', { replace: true });
@@ -25,20 +21,7 @@ export const LoginRoute = () => {
 
   return (
     <AuthLayout title="MauaGrid">
-      {accounts.length > 0 ? (
-        <div className="text-center">
-          <p>Welcome, {accounts[0].name}</p>
-          <p>Username: {accounts[0].username}</p>
-          <button
-            className="mt-4 rounded bg-gray-200 px-4 transition duration-300 hover:bg-blue-400 hover:text-white"
-            onClick={handleLogout}
-          >
-            Logout
-          </button>
-        </div>
-      ) : (
-        <LoginForm onSuccess={() => handleLogin()} />
-      )}
+      <LoginForm onSuccess={() => handleLogin()} />
     </AuthLayout>
   );
 };
